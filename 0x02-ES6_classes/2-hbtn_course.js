4002ce9fb5499ba749c9900ad3d7c2b3e849ca4d
ShehabNegm <shbonegm@gmail.com>
2023-11-30 23:13:23 +0200

    add 2nd task

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+class HolbertonCourse {
+  constructor (name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name
+    }
+    if (typeof length === 'number') {
+      this._length = length
+    }
+    if (Array.isArray(students)) {
+      this._students = students
+    }
+  }
+
+  get name () {
+    return this.__name
+  }
+
+  get length () {
+    return this._length
+  }
+
+  get students () {
+    return this._students
+  }
+
+  set name (test) {
+    if (typeof test === 'string') {
+      this._name = test
+    }
+  }
+
+  set length (no) {
+    if (typeof no === 'number') {
+      this._length = no
+    }
+  }
+
+  set students (stu) {
+    if (Array.isArray(stu)) {
+      this._students = stu
+    }
+  }
+}
